91204a73025f40a19eb3d93008b5128a7e6dd0f2
Jonathan Bachelor <jon_bachelor@me.com>
2016-08-19 21:48:22 -0700

    Adding content to main phone word view via code. Translate button is all hooked up. Call button enables & disables as it should, but does not yet try to make a call.

--- PhoneWordxPlatform/MainPage.cs
+++ PhoneWordxPlatform/MainPage.cs
@@ -6,18 +6,64 @@ namespace PhoneWordxPlatform
 {
 	public class MainPage : ContentPage
 	{
+		Entry phoneNumberText;
+		Button translateButton;
+		Button callButton;
+
+		string translatedNumber = string.Empty;
+		string originalNumber = string.Empty;
+
 		public MainPage()
 		{
-			Content = new StackLayout
+			this.Padding = new Thickness(20, Device.OnPlatform<double>(40, 20, 20), 20, 20);
+
+			StackLayout panel = new StackLayout
 			{
-				VerticalOptions = LayoutOptions.Center,
-				Children = {
-					new Label {
-						HorizontalTextAlignment = TextAlignment.Center,
-						Text = "Welcome to Xamarin Forms!"
-					}
-				}
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Orientation = StackOrientation.Vertical,
+				Spacing = 15
 			};
+
+			panel.Children.Add(new Label
+			{
+				Text = "Enter a phoneword:"
+			});
+
+			panel.Children.Add(phoneNumberText = new Entry
+			{
+				Placeholder = "1-800-PUGLOVE"
+			});
+
+			panel.Children.Add(translateButton = new Button
+			{
+				Text = "Translate"
+			});
+
+			panel.Children.Add(callButton = new Button
+			{
+				Text = "Call",
+				IsEnabled = false
+			});
+
+			translateButton.Clicked += OnTranslate;
+			this.Content = panel;
+		}
+
+		void OnTranslate(object sender, EventArgs e)
+		{
+			originalNumber = phoneNumberText.Text;
+			translatedNumber = PhonewordTranslator.ToNumber(originalNumber);
+
+			if (string.IsNullOrWhiteSpace(translatedNumber))
+			{
+				callButton.Text = "Call";
+				callButton.IsEnabled = false;
+			}
+			else {
+				callButton.Text = $"Call {translatedNumber}";
+				callButton.IsEnabled = true;
+			}
 		}
 	}
 }
